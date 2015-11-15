0e4f7964dee000aab716a7900fdb08c18755b046
Michael Dyrynda <michael@iatstuti.net>
2015-11-15 15:30:48 +1030

    Remove cart total from secondary menu

--- web/app/themes/foods-for-life/header.php
+++ web/app/themes/foods-for-life/header.php
@@ -92,8 +92,6 @@
                     if ( '' !== $et_secondary_nav ) {
                         echo $et_secondary_nav;
                     }
-
-                    et_show_cart_total();
                     ?>
                 </div> <!-- #et-secondary-menu -->
 
