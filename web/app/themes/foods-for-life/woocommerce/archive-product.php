7464c9431f7782adf5ac95b329a2a578102f38e7
Michael Dyrynda <michael@iatstuti.net>
2015-11-23 21:31:33 +1030

    Adjust display of the product archive template
    Fixes #19
    
    Wrap the default archive-content template in our FFL 3/4 - 1/4 column
    layout, adding the product category sidebar to the template.

--- web/app/themes/foods-for-life/woocommerce/archive-product.php
+++ web/app/themes/foods-for-life/woocommerce/archive-product.php
@@ -13,91 +13,95 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
 
-do_action('render_heading', array( 'Specials', ));
+get_header( 'shop' );
 
-get_header( 'shop' ); ?>
+render_heading('Specials');
+?>
 
-    <?php
-        /**
-         * woocommerce_before_main_content hook
-         *
-         * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-         * @hooked woocommerce_breadcrumb - 20
-         */
-        do_action( 'woocommerce_before_main_content', 'Specials' );
-    ?>
-
-        <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-
-            <h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
-
-        <?php endif; ?>
-
-        <?php
-            /**
-             * woocommerce_archive_description hook
-             *
-             * @hooked woocommerce_taxonomy_archive_description - 10
-             * @hooked woocommerce_product_archive_description - 10
-             */
-            do_action( 'woocommerce_archive_description' );
-        ?>
-
-        <?php if ( have_posts() ) : ?>
+<div class="et_pb_section specials__content et_pb_section_1 et_pb_with_background et_section_regular">
+    <div class="et_pb_row et_pb_row_0 et_pb_equal_columns et_pb_row_3-4_1-4">
+        <div class="et_pb_column et_pb_column_3_4 et_pb_column_0">
 
             <?php
                 /**
-                 * woocommerce_before_shop_loop hook
+                 * woocommerce_before_main_content hook
                  *
-                 * @hooked woocommerce_result_count - 20
-                 * @hooked woocommerce_catalog_ordering - 30
+                 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+                 * @hooked woocommerce_breadcrumb - 20
                  */
-                do_action( 'woocommerce_before_shop_loop' );
+                do_action( 'woocommerce_before_main_content', 'Specials' );
             ?>
 
-            <?php woocommerce_product_loop_start(); ?>
+                <?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 
-                <?php woocommerce_product_subcategories(); ?>
+                    <h1 class="page-title"><?php woocommerce_page_title(); ?></h1>
 
-                <?php while ( have_posts() ) : the_post(); ?>
+                <?php endif; ?>
 
-                    <?php wc_get_template_part( 'content', 'product' ); ?>
+                <?php
+                    /**
+                     * woocommerce_archive_description hook
+                     *
+                     * @hooked woocommerce_taxonomy_archive_description - 10
+                     * @hooked woocommerce_product_archive_description - 10
+                     */
+                    do_action( 'woocommerce_archive_description' );
+                ?>
 
-                <?php endwhile; // end of the loop. ?>
+                <?php if ( have_posts() ) : ?>
 
-            <?php woocommerce_product_loop_end(); ?>
+                    <?php
+                        /**
+                         * woocommerce_before_shop_loop hook
+                         *
+                         * @hooked woocommerce_result_count - 20
+                         * @hooked woocommerce_catalog_ordering - 30
+                         */
+                        do_action( 'woocommerce_before_shop_loop' );
+                    ?>
+
+                    <?php woocommerce_product_loop_start(); ?>
+
+                        <?php woocommerce_product_subcategories(); ?>
+
+                        <?php while ( have_posts() ) : the_post(); ?>
+
+                            <?php wc_get_template_part( 'content', 'product' ); ?>
+
+                        <?php endwhile; // end of the loop. ?>
+
+                    <?php woocommerce_product_loop_end(); ?>
+
+                    <?php
+                        /**
+                         * woocommerce_after_shop_loop hook
+                         *
+                         * @hooked woocommerce_pagination - 10
+                         */
+                        do_action( 'woocommerce_after_shop_loop' );
+                    ?>
+
+                <?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+
+                    <?php wc_get_template( 'loop/no-products-found.php' ); ?>
+
+                <?php endif; ?>
 
             <?php
                 /**
-                 * woocommerce_after_shop_loop hook
+                 * woocommerce_after_main_content hook
                  *
-                 * @hooked woocommerce_pagination - 10
+                 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
                  */
-                do_action( 'woocommerce_after_shop_loop' );
+                do_action( 'woocommerce_after_main_content' );
             ?>
-
-        <?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-
-            <?php wc_get_template( 'loop/no-products-found.php' ); ?>
-
-        <?php endif; ?>
-
-    <?php
-        /**
-         * woocommerce_after_main_content hook
-         *
-         * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-         */
-        do_action( 'woocommerce_after_main_content' );
-    ?>
-
-    <?php
-        /**
-         * woocommerce_sidebar hook
-         *
-         * @hooked woocommerce_get_sidebar - 10
-         */
-        do_action( 'woocommerce_sidebar' );
-    ?>
+        </div> <!-- .et_pb_column -->
+        <div class="et_pb_column et_pb_column_1_4 et_pb_column_1">
+            <div class="et_pb_widget_area et_pb_widget_area_left clearfix et_pb_module et_pb_bg_layout_light  et_pb_sidebar_0 et_pb_sidebar_no_border">
+                <?php dynamic_sidebar( 'ffl-specials-sidebar' ); ?>
+            </div> <!-- .et_pb_widget_area -->
+        </div> <!-- .et_pb_column -->
+    </div> <!-- .et_pb_row -->
+</div>
 
 <?php get_footer( 'shop' ); ?>
