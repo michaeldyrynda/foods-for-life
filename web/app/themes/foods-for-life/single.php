6563347b14d95ef86f0eaac1a665bd823304e2ee
Michael Dyrynda <michael@iatstuti.net>
2015-12-01 05:16:18 +1030

    Remove testing header

--- web/app/themes/foods-for-life/single.php
+++ web/app/themes/foods-for-life/single.php
@@ -17,7 +17,6 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
                         <?php if (et_get_option('divi_integration_single_top') <> '' && et_get_option('divi_integrate_singletop_enable') == 'on') echo(et_get_option('divi_integration_single_top')); ?>
 
                         <article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
-                            <h1>testing</h1>
                             <?php if ( ( 'off' !== $show_default_title && $is_page_builder_used ) || ! $is_page_builder_used ) { ?>
                                 <div class="et_post_meta_wrapper">
                                     <?php
